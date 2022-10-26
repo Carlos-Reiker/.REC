ada8f99d3e928c2f8ea9b158a3b3eab4cd751463
Carlos Reiker <carlos.reiker@hotmail.com>
2022-10-25 23:06:55 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
 const fileSelector = document.getElementById('myfile');
 
-fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    console.log(fileList);});
+fileSelector.addEventListener
