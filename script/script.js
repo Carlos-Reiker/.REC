79a07b1af6a637823082f3710791a1c859b15e44
Carlos Reiker <carlos.reiker@hotmail.com>
2022-10-27 13:43:47 -0300

    Testes 27/10
    Testes de funcionalidade para subir e exibir vídeo.

--- script/script.js
+++ script/script.js
@@ -8,3 +8,18 @@ function newDoc() {
     window.location.assign("myfile").value;
   }*/
 
+var div = document.getElementsByClassName("botaoArquivo")[0];
+var input = document.getElementById("inputArquivo");
+
+div.addEventListener("click", function(){
+    input.click();
+});
+
+input.addEventListener("change", function(){
+    var nome = "Não há arquivo selecionado. Selecionar arquivo...";
+    var exibirVideo = document.querySelector("#playerVideo");
+
+    if(input.files.length > 0) nome = input.files[0].name;
+    exibirVideo.setAttribute('src', nome);
+}
+)
