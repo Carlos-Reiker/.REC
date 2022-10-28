acb2bf61bbb606acbf7ccb5f6a399619bfcb8146
Carlos Reiker <carlos.reiker@hotmail.com>
2022-10-28 18:15:52 -0300

    28/10

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+/*var video = document.getElementById('playerVideo');
+var source = document.getElementById('videoSrc');
+
+source.setAttribute('src', '/design/18.mp4');
+
+video.load();
+video.play();*/
+
+
+
