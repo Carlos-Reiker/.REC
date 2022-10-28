c1b79400bc574028784e5fe4e8aa65e56a66f1bb
Carlos Reiker <carlos.reiker@hotmail.com>
2022-10-27 21:51:10 -0300

    27/10 Teste

--- script/script.js
+++ script/script.js
@@ -8,18 +8,20 @@ function newDoc() {
     window.location.assign("myfile").value;
   }*/
 
-var div = document.getElementsByClassName("botaoArquivo")[0];
-var input = document.getElementById("inputArquivo");
+var div = document.getElementsByClassName("botaoArquivo");
+var input = document.getElementById("inputArquivo").value;
 
-div.addEventListener("click", function(){
+/*div.addEventListener("click", function(){ 
     input.click();
-});
+});*/
 
 input.addEventListener("change", function(){
     var nome = "Não há arquivo selecionado. Selecionar arquivo...";
     var exibirVideo = document.querySelector("#playerVideo");
 
-    if(input.files.length > 0) nome = input.files[0].name;
+    if(input.files.length > 0) 
+    nome = input.files[0].name;
     exibirVideo.setAttribute('src', nome);
 }
 )
+console.log(div.value);
